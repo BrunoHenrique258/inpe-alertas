0107668d78e10c2291cbbdf069f1a45167b5a018
BrunoHenrique258 <bruno.oliveira258@fatec.sp.gov.br>
2025-10-16 20:47:25 -0300

    Resolução da atividade da aula 8

---
+++ src/alerta.js
@@ -0,0 +1,13 @@
+function classificarAlerta(valor) {
+    if (valor >= 90) {
+        return "Crítico";
+    } else if (valor >= 70) {
+        return "Alto";
+    } else if (valor >= 40) {
+        return "Médio";
+    } else {
+        return "Baixo";
+    }
+}
+
+module.exports = { classificarAlerta };
---
+++ src/tests/alerta.test.js
@@ -0,0 +1,5 @@
+const { classificarAlerta } = require('../alerta');
+
+test("alerta alto", () => {
+    expect(classificarAlerta(70)).toBe("Alto");
+});
